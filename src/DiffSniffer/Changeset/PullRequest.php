7ce210889bb3f99d57c0190dac331ccbd3b6db2b
Sergei Morozov <morozov@tut.by>
2013-08-03 17:22:52 +0300

    Extracted pull request validator into separate project

---
+++ src/DiffSniffer/Changeset/PullRequest.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Changeset that represents pull request on GitHub
+ *
+ * PHP version 5
+ *
+ * @category  DiffSniffer
+ * @package   DiffSniffer
+ * @author    Sergei Morozov <morozov@tut.by>
+ * @copyright 2013 Sergei Morozov
+ * @license   http://mit-license.org/ MIT Licence
+ * @link      http://github.com/morozov/diff-sniffer
+ */
+namespace DiffSniffer\Changeset;
+
+use DiffSniffer\Changeset;
+use Github\Client;
+
+/**
+ * Changeset that represents pull request on GitHub
+ *
+ * PHP version 5
+ *
+ * @category  DiffSniffer
+ * @package   DiffSniffer
+ * @author    Sergei Morozov <morozov@tut.by>
+ * @copyright 2012 Sergei Morozov
+ * @license   http://mit-license.org/ MIT Licence
+ * @link      http://github.com/morozov/diff-sniffer
+ */
+class PullRequest implements Changeset
+{
+    /**
+     * GitHub API client
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * GitHub user name
+     *
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * GitHub repository
+     *
+     * @var string
+     */
+    protected $repo;
+
+    /**
+     * GitHub pull request ID
+     *
+     * @var int
+     */
+    protected $pull;
+
+    /**
+     * Constructor
+     *
+     * @param Client $client GitHub API client
+     * @param string $user   GitHub user name
+     * @param string $repo   GitHub repository
+     * @param string $pull   GitHub pull request ID
+     */
+    public function __construct(Client $client, $user, $repo, $pull)
+    {
+        $this->client = $client;
+        $this->user = $user;
+        $this->repo = $repo;
+        $this->pull = $pull;
+    }
+
+    /**
+     * Returns diff of the changeset
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function getDiff()
+    {
+        /** @var \Github\Api\PullRequest $api */
+        $api = $this->client->api('pull_request');
+        $this->client->setHeaders(
+            array(
+                sprintf(
+                    'Accept: application/vnd.github.%s.diff',
+                    $this->client->getOption('api_version')
+                )
+            )
+        );
+
+        $diff = $api->show($this->user, $this->repo, $this->pull);
+
+        return $diff;
+    }
+
+    /**
+     * Exports the changed files into specified directory
+     *
+     * @param string $dir Target directory
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function export($dir)
+    {
+        /** @var \Github\Api\PullRequest $api */
+        $api = $this->client->api('pull_request');
+        $files = $api->files($this->user, $this->repo, $this->pull);
+
+        /** @var \Github\Api\GitData $api */
+        $api = $this->client->api('git_data');
+        $api = $api->blobs();
+        /** @var \Github\Api\GitData\Blobs $api */
+        $api->configure('raw');
+        foreach ($files as $file) {
+            $contents = $api->show($this->user, $this->repo, $file['sha']);
+            $path = $dir . '/' . $file['filename'];
+            $dirName = dirname($path);
+            if (!file_exists($dirName)) {
+                mkdir(dirname($path), 0777, true);
+            }
+            file_put_contents($path, $contents);
+        }
+    }
+}
---
+++ src/pull-request.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Github pull request validation script
+ *
+ * PHP version 5
+ *
+ * @category  DiffSniffer
+ * @package   DiffSniffer
+ * @author    Sergei Morozov <morozov@tut.by>
+ * @copyright 2013 Sergei Morozov
+ * @license   http://mit-license.org/ MIT Licence
+ * @link      http://github.com/morozov/diff-sniffer
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (!file_exists(__DIR__ . '/../config.php')) {
+    throw new \LogicException(
+        'Configuration file doesn\'t exist. Please run authenticate.php before'
+    );
+}
+
+if ($_SERVER['argc'] < 4) {
+    throw new \InvalidArgumentException(
+        'Usage: ' . $_SERVER['argv'][0] . ' user repo pull <code sniffer arguments>'
+    );
+}
+
+$config = include __DIR__ . '/../config.php';
+
+$client = new Github\Client(
+    new Github\HttpClient\CachedHttpClient(
+        array(
+            'cache_dir' => '/tmp/github-api-cache',
+        )
+    )
+);
+
+$client->authenticate($config['token'], null, Github\Client::AUTH_URL_TOKEN);
+
+$arguments = $_SERVER['argv'];
+array_shift($arguments);
+
+$changeset = new \DiffSniffer\Changeset\PullRequest(
+    $client,
+    array_shift($arguments),
+    array_shift($arguments),
+    array_shift($arguments)
+);
+
+$projectRoot = dirname(__DIR__);
+
+$runner = new \DiffSniffer\Runner();
+$return_var = $runner->run($changeset, $arguments);
+
+exit($return_var);
