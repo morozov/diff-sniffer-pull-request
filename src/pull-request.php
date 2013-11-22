17c4bbbe815adf1a70ac5eeabf955ff73e08a1f3
Sergei Morozov <morozov@tut.by>
2013-11-22 22:54:22 +0300

    Check whether application is installed

--- src/pull-request.php
+++ src/pull-request.php
@@ -12,7 +12,17 @@
  * @license   http://mit-license.org/ MIT Licence
  * @link      http://github.com/morozov/diff-sniffer
  */
-require_once __DIR__ . '/../vendor/autoload.php';
+$autoload = __DIR__ . '/../vendor/autoload.php';
+
+if (!file_exists($autoload)) {
+    echo 'You must set up the project dependencies, run the following commands:'
+        . PHP_EOL . 'curl -sS https://getcomposer.org/installer | php'
+        . PHP_EOL . 'php composer.phar install'
+        . PHP_EOL;
+    exit(-1);
+}
+
+require $autoload;
 
 $client = new Github\Client(
     new Github\HttpClient\CachedHttpClient(
